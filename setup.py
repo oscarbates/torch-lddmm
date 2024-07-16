8cfb7ac518205c627a8ebeee14fef01fe1b5452e
Oscar Bates <oscar.bates@live.co.uk>
2024-07-16 16:05:49 +0100

    Allowing setup via pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='torch_lddmm',
+    version='0.1',
+    py_modules=['torch_lddmm'],
+    author='',
+    author_email='',
+    description='A fork of torch-lddmm',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/oscarbates/torch-lddmm',
+    license='',
+)
